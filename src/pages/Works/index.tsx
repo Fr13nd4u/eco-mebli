3241b9d1b796ae7d93892cd53dee02636eeee1d5
Msrichka <mariadikhtiievska@gmail.com>
2022-05-25 16:29:30 +0300

    Works page updated

--- src/pages/Works/index.tsx
+++ src/pages/Works/index.tsx
@@ -1,122 +1,59 @@
 import React from "react";
 import { Link } from "react-router-dom";
-
-import item1 from "../../assets/works_images/kitchen.png";
-import item2 from "../../assets/works_images/bathroom.jpg";
-import item3 from "../../assets/works_images/3.jpg";
-import item4 from "../../assets/works_images/tables.jpg";
-import item5 from "../../assets/works_images/stairs.jpg";
-import item6 from "../../assets/works_images/light.jpg";
-import item7 from "../../assets/works_images/doors.jpg";
 import "./Works.scss";
+import WorkOption from "../../components/WorkOption";
+
+const images = [
+  {
+    image: "/assets/works_images/kitchen.png",
+    title: "КУХНІ",
+  },
+  {
+    image: "/assets/works_images/bathroom.jpg",
+    title: "ШУХЛЯДКИ",
+  },
+  {
+    image: "/assets/works_images/3.jpg",
+    title: "ШУХЛЯДКИ",
+  },
+  {
+    image: "/assets/works_images/tables.jpg",
+    title: "Столи",
+  },
+  {
+    image: "/assets/works_images/stairs.jpg",
+    title: "СХОДИ",
+  },
+  {
+    image: "/assets/works_images/light.jpg",
+    title: "СВІТЛО",
+  },
+  {
+    image: "/assets/works_images/doors.jpg",
+    title: "ДВЕРІ",
+  },
+];
 const Works = () => {
   return (
     <section>
-      <div className="works_container">
-        <div className="apper_container"></div>
+      <div className="apper_container"></div>
+      <div className="container">
+      
         <p className="apper_container-title">РОБОТИ</p>
         <div className="options_grid">
-          <Link
-            to="/"
-            className="option top_option"
-            style={{
-              backgroundImage: `url(${item1})`,
-            }}
-          >
-            <div>
-              <span className="option_header">
-                <p>КУХНІ</p>
-              </span>
-            </div>
-          </Link>
-
-          <Link
-            to="/"
-            className="option else_option"
-            style={{
-              backgroundImage: `url(${item2})`,
-            }}
-          >
-            <div style={{ position: "relative" }}>
-              <span className="option_header">
-                <p>ШУХЛЯДКИ</p>
-              </span>
-            </div>
-          </Link>
-          <Link
-            to="/"
-            className="option else_option"
-            style={{
-              backgroundImage: `url(${item3})`,
-            }}
-          >
-            <div style={{ position: "relative" }}>
-              <span className="option_header">
-                <p>ШУХЛЯДКИ</p>
-              </span>
-            </div>
-          </Link>
-
-          <Link
-            to="/"
-            className="option else_option"
-            style={{
-              backgroundImage: `url(${item4})`,
-            }}
-          >
-            <div style={{ position: "relative" }}>
-              <span className="option_header">
-                <p>Столи</p>
-              </span>
-            </div>
-          </Link>
-
-          <Link
-            to="/"
-            className="option else_option"
-            style={{
-              backgroundImage: `url(${item5})`,
-            }}
-          >
-            <div style={{ position: "relative" }}>
-              <span className="option_header">
-                <p>СХОДИ</p>
-              </span>
-            </div>
-          </Link>
-
-          <Link
-            to="/"
-            className="option else_option"
-            style={{
-              backgroundImage: `url(${item6})`,
-            }}
-          >
-            <div style={{ position: "relative" }}>
-              <span className="option_header">
-                <p>СХОДИ</p>
-              </span>
-            </div>
-          </Link>
-
-          <Link
-            to="/"
-            className="option else_option"
-            style={{
-              backgroundImage: `url(${item7})`,
-            }}
-          >
-            <div style={{ position: "relative" }}>
-              <span className="option_header">
-                <p>СХОДИ</p>
-              </span>
-            </div>
-          </Link>
+          {images.map((item) => (
+            <WorkOption
+              key={item.image}
+              imageURL={item.image}
+              headerClssName="option_header"
+              backgroundClassName={
+                item === images[0] ? "option top_option" : "option else_option"
+              }
+              title={item.title}
+            />
+          ))}
         </div>
-
-      
       </div>
-     
     </section>
   );
 };
